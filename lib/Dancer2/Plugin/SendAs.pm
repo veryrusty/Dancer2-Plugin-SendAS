0815570f3a5b0c42c0f83b5d5d344b35ec83ab0a
Russell Jenkins <veryrusty@gmail.com>
2015-10-21 12:36:10 +0200

    Add dzil scaffolding

--- lib/Dancer2/Plugin/SendAs.pm
+++ lib/Dancer2/Plugin/SendAs.pm
@@ -1,5 +1,5 @@
 package Dancer2::Plugin::SendAs;
-# ABSTRACT Dancer2 plugin to send data as specific content type
+# ABSTRACT: Dancer2 plugin to send data as specific content type
 
 use strict;
 use warnings;
@@ -9,6 +9,8 @@ use Dancer2::Plugin;
 use Class::Load 'try_load_class';
 use Encode;
 use List::Util 'first';
+
+# VERSION
 
 register send_as => sub {
     my ( $dsl, $type, $data ) = @_;
@@ -51,12 +53,13 @@ register_plugin;
     set serializer => 'YAML';
     set template => 'template_toolkit';
 
-    get '/html' => {
+    get '/html' => sub {
         send_as html => template 'foo';
-    }
+    };
 
     get '/json/**' => sub {
         send_as json => splat;
     };
 
 =cut
+
